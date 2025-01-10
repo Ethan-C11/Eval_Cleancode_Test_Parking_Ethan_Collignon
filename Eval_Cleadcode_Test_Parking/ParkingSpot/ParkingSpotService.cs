bbe88438d2dd05c790f148c8c5d7df8e9656b23e
Ethan-C11 <ethan.collignon.1102@gmail.com>
2025-01-10 15:17:31 +0100

    Ajout projet tests & renommage

---
+++ Eval_Cleadcode_Test_Parking/ParkingSpot/ParkingSpotService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eval_Cleadcode_Test_Parking.ParkingSpot
+{
+    public class ParkingSpotService
+    {
+    }
+}
---
+++ Parking_UnitTest/ParkingTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eval_Cleadcode_Test_Parking.Parking;
+
+namespace Parking_UnitTest
+{
+    [TestClass]
+    public class ParkingTest
+    {
+        private ParkingService _parkingService;
+
+        [TestInitialize]
+        void InitializeTests()
+        {
+
+        }
+    }
+}
