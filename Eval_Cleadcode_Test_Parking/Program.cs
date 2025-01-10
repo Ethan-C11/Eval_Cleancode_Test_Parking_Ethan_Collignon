3156dd2f1ffd53f9f33a72b39509d91cf54cfbef
Ethan-C11 <ethan.collignon.1102@gmail.com>
2025-01-10 15:08:53 +0100

    first commit

---
+++ Eval_Cleadcode_Test_Parking/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Eval_Cleadcode_Test_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
